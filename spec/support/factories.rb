157c9dbaa1fb838e4426c5dd37011fe1348ce9df
Gregory Tomei <gregory@websystemstudio.com>
2010-12-16 17:46:02 -0800

    fixing tests, using sequence in the factories

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,4 +1,3 @@
-
 Factory.define :element do |f| 
   f.name 'On the Electrodynamics of Moving Bodies'
   f.body  'Some very important words.'
@@ -6,16 +5,15 @@ Factory.define :element do |f|
 end
 
 Factory.define :user do |f|
-  f.login  'log_name1'           
-  f.crypted_password 'secret' 
+  f.sequence(:login) {|n| "login#{n}" }
+  f.crypted_password 'secret'
   f.password 'secret'
   f.password_confirmation 'secret'
-  f.password_salt 'secret'    
-  f.persistence_token 
+  f.password_salt 'secret'
+  f.persistence_token
   f.perishable_token
   f.association :account
 end
 
 Factory.define :account do |f| 
- 
 end
--- spec/views/elements/index.html.erb_spec.rb
+++ spec/views/elements/index.html.erb_spec.rb
@@ -2,23 +2,15 @@ require 'spec_helper'
 
 describe "elements/index.html.erb" do
   before(:each) do
-    assign(:elements, [Factory.create(:element), Factory.create(:element)])
-      # stub_model(Element,
-      #   :name => "Name",
-      #   :body => "Body",
-      #   :user_id => "User"
-      # ),
-      # stub_model(Element,
-      #   :name => "Name",
-      #   :body => "Body",
-      #   :user_id => "User"
-      # )
+    assigns[:elements] = @elements = [Factory.create(:element), Factory.create(:element)]
   end
 
   it "renders a list of elements" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Body".to_s, :count => 2
-    assert_select "tr>td", :text => "User".to_s, :count => 2
+    @elements.each do |element|
+      assert_select "tr>td", :text => element.name
+      assert_select "tr>td", :text => element.body
+      assert_select "tr>td", :text => element.owner.login
+    end
   end
 end

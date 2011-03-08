0cc94d7776470dd34fe1714b3702463d8e6e2e49
Jeff Bolton <jeffreyebolton@gmail.com>
2011-03-08 14:37:40 -0800

    fixed localhost problem, by removing line from config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-CmsTool::Application.config.session_store :cookie_store, :key => '_cms_tool_session',  :domain => :all  
-
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
